37c3264d651b2809fd3025717bea6989e49c73a6
Michal Schorm <mschorm@redhat.com>
2020-03-23 11:59:01 +0100

    Tweak autorun

--- autorun.sh
+++ autorun.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+#----------------------------------------
+# SET UP A RELATIVE PATH
+
+# Determine and set the relative path we are on
+#   $BASH_SOURCE is the only trustworthy source of information. But it does not follow symlinks - use readlink to get canonical path
+relative_path=$( readlink -f "$BASH_SOURCE" )
+# Move to the current script location, to assure relative paths will be configured correctly
+pushd "${relative_path%/*}" || exit
+
+#----------------------------------------
+# RUN ALL THE AUTORUN SCRIPTS
 
 set -vx
 
@@ -5,3 +18,7 @@ sh 10_security_hardening.sh
 sh 50_system_configuration.sh
 sh 60_x_server.sh
 
+#----------------------------------------
+
+# Jump back to the directory we were before this script execution
+popd
