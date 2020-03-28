e1bf1d81c1cc82296ff6eeaf41a36185bc9ac54f
UDOO <udoo@udoo.server>
2020-03-28 13:05:16 +0100

    Fix the sound execution as user 'lod'

--- 70_audio.sh
+++ 70_audio.sh
@@ -14,11 +14,22 @@ dnf install -y pulseaudio pulseaudio-utils alsa-plugins-pulseaudio pulseaudio-mo
 # It offers a user systemd service, which can be started by all (non-root) users as following:
 #   systemctl --user start pulseaudio
 
+# First add users that are expected to run sound to 'audio' group
 usermod -a -G audio udoo
 usermod -a -G audio lod
-#   systemctl --user start pulseaudio
-su -c "systemctl --user enable pulseaudio" udoo
-su -c "systemctl --user enable pulseaudio" lod
+# Enable pulseaudio service for all interactive users
+systemctl --global enable pulseaudio
 
+# Update ".bash_profile" configuration of user 'lod', so it will set up correctly DBUS environment variables on each login (bash execution)
+su -c "cat" lod << EOF >> /home/lod/.bash_profile
+export XDG_SESSION_CLASS=user
+export XDG_RUNTIME_DIR=/run/user/1001
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1001/bus
+EOF
+
+# An user manager is spawned for the user at boot and kept around after logouts. This allows users who are not logged in to run long-running services.
+loginctl enable-linger lod
+
+# From now, it is possible to run a sound as as 'lod' user, once a login operation was performed and evironment variables vere set (e.g. "su -" bot not "su").
 
 # ------------------------------------------------------------
